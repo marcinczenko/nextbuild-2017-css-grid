39f0e670b749e6e00889145df8729f0f71fa8935
Czenko, Marcin <marcin.czenko@philips.com>
2017-05-19 02:44:35 +0200

    CSS moved to javascript.

--- src/App.js
+++ src/App.js
@@ -1,21 +1,73 @@
 import React, { Component } from 'react';
-import './App.css';
+
 
 class App extends Component {
 
+  constructor() {
+    super();
+    this.styles = styles;
+  }
+
   render() {
     return (
-      <div className="container">
-        <section className="header">header</section>
-        <section className="horizontal-menu">horizontal-menu</section>
-        <section className="side-panel-left">side-panel-left</section>
-        <section className="content">content</section>
-        <section className="side-panel-right">side-panel-right</section>
-        <section className="messanger">messanger</section>
-        <section className="status-bar">status-bar</section>
+      <div style={this.styles.container}>
+        <section style={{...this.styles.section, ...this.styles.header}}>header</section>
+        <section style={{...this.styles.section, ...this.styles.horizontalMenu}}>horizontal-menu</section>
+        <section style={{...this.styles.section, ...this.styles.sidePanelLeft}}>side-panel-left</section>
+        <section style={{...this.styles.section, ...this.styles.content}}>content</section>
+        <section style={{...this.styles.section, ...this.styles.sidePanelRight}}>side-panel-right</section>
+        <section style={{...this.styles.section, ...this.styles.messanger}}>messanger</section>
+        <section style={{...this.styles.section, ...this.styles.statusBar}}>status-bar</section>
       </div>
     );
   }
 }
+
+const styles = {
+  container: {
+    display: 'grid',
+    gridGap: '5px',
+    gridTemplateAreas: '"a a a" "b b b" "c d e" "c d e" "c d f" "g g f"',
+    gridTemplateColumns: '1fr 1fr 1fr',
+    gridTemplateRows: '1fr 1fr 1fr 1fr 1fr 1fr 1fr'
+  },
+
+  section: {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    fontSize: '28px',
+    backgroundColor: 'CadetBlue',
+    padding: '10px'
+  },
+
+  header: {
+    gridArea: 'a'
+  },
+
+  horizontalMenu: {
+    gridArea: 'b'
+  },
+
+  sidePanelLeft: {
+    gridArea: 'c'
+  },
+
+  content: {
+    gridArea: 'd'
+  },
+
+  sidePanelRight: {
+    gridArea: 'e'
+  },
+
+  messanger: {
+    gridArea: 'f'
+  },
+
+  statusBar: {
+    gridArea: 'g'
+  }
+};
 
 export default App;
